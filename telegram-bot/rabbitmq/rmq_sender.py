daaa08f5d78109e7ce0bbb341f2716fde4de9aec
AlexisOMG <alesha-gramazeka@yandex.ru>
2023-11-13 23:39:20 +0300

    add rmq listener and sender

---
+++ telegram-bot/rabbitmq/rmq_sender.py
@@ -0,0 +1,24 @@
+import pika
+
+class RMQSender:
+    def __init__(self, queue: str, host: str, port: str, user: str, password: str) -> None:
+        self.queue = queue
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
+            host=host, 
+            port=port, 
+            credentials=pika.PlainCredentials(username=user, password=password),
+        ))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=queue, durable=True)
+
+    def send_message(self, request_id: int, msg: str) -> None:
+        message = {'request_id': request_id, 'message': msg}
+        self.channel.basic_publish(exchange='',
+                                   routing_key=self.queue,
+                                   body=str(message),
+                                   properties=pika.BasicProperties(
+                                       delivery_mode=1,
+                                   ))
+
+    def close_connection(self) -> None:
+        self.connection.close()
