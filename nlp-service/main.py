29158c90c311f0df6ed4ae2a419b7377160436dc
Mikhail Elenskii <elenskii-mikhail@outlook.com>
2023-11-15 04:38:27 +0500

    Revert "chore(nlp-service): reformat imports in `main.py`"
    This reverts commit f9efb805eb54ed1cee2fb005680fb3e04a148268.

--- nlp-service/main.py
+++ nlp-service/main.py
@@ -15,8 +15,8 @@ from common import (
     QDRANT_COLLECTION_NAME_ENVAR_KEY_NAME,
     RESPONSE_ADDRESS_KEY,
     RESPONSE_TYPE_KEY,
-    RMQ_OUTPUT_PORT_ENVAR_KEY_NAME,
     ServiceResponse,
+    RMQ_OUTPUT_PORT_ENVAR_KEY_NAME,
     ServiceResponseBody,
     ServiceRequest,
 )
