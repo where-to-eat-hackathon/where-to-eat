a8de017ca97ccea2e16e81bf26144cb3294f86e9
Mikhail Elenskii <9024831+mkls6@users.noreply.github.com>
2023-11-16 02:28:08 +0500

    Add initial geotagger support in nlp-service, support additional fields in request/response payloads (#5)
    * add service logic
    
    * support geocoding
    
    * support finding nearest geocode
    
    * delete old files
    
    * add full-text filtration for db queries
    
    * fixed format
    
    * fix(nlp-service): make `ServiceRequest.town` optional
    
    * chore(helm): update `nlp-service` image
    
    * fix(nlp-service): make Optional fields `None` by default
    
    * chore(helm): update `nlp-service` image to `python-service_ed477f`
    
    * fix(nlp-service): typo in output queue name
    
    * chore(helm): update `nlp-service` image
    
    * fix(helm): swap queue names in `tg-bot`
    
    * fix(helm): add missing `RMQ_OUTPUT_PORT`
    
    * SOLVED THIS FUCKING STUPID BUG, THANKS GUYS!!!!!!!!!!!
    
    * RENAMED FIELDS. SHIT. FUCK. FUCK.
    
    * chore(helm): update nlp-service image to python-service_517128
    
    * fix(helm): correctly set `RMQ_PORT` environment variable
    
    ---------
    
    Co-authored-by: EmirVildanov <reddog201030@gmail.com>

--- nlp-service/common.py
+++ nlp-service/common.py
@@ -1,13 +1,14 @@
 # ENVIRONMENT VARIABLES.
 from dataclasses import dataclass
+from typing import Optional, List
+
 from dataclasses_json import dataclass_json
 
 RMQ_URL_ENVAR_KEY_NAME = "RMQ_URL"
 # Queue that accepts requests FROM Telegram bot.
-RMQ_INPUT_PORT_ENVAR_KEY_NAME = "RMQ_INPUT_PORT"
+RMQ_PORT_ENVAR_KEY_NAME = "RMQ_PORT"
 INPUT_QUEUE_NAME_ENVAR_KEY_NAME = "INPUT_QUEUE_NAME"
 # Queue that accepts requests FOR Telegram bot.
-RMQ_OUTPUT_PORT_ENVAR_KEY_NAME = "RMQ_INPUT_PORT"
 OUTPUT_QUEUE_NAME_ENVAR_KEY_NAME = "OUTPUT_QUEUE_NAME"
 RMQ_USERNAME_ENVAR_KEY_NAME = "RMQ_USERNAME"
 RMQ_PASSWORD_ENVAR_KEY_NAME = "RMQ_PASSWORD"
@@ -19,16 +20,29 @@ QDRANT_COLLECTION_NAME_ENVAR_KEY_NAME = "QDRANT_COLLECTION_NAME"
 # QDRANT COLLECTION FIELDS (also used as JSON fields).
 RESPONSE_ADDRESS_KEY = "address"
 RESPONSE_NAME_KEY = "name"
-RESPONSE_TYPE_KEY = "type"
+RESPONSE_TYPE_KEY = "rubrics"
+RESPONSE_RATING_KEY = "rating"
+RESPONSE_TEXT_KEY = "text"
+
+
+@dataclass_json
+@dataclass
+class GeocodedAddress:
+    """Text address transformed into coordinates."""
+
+    latitude: float
+    longitude: float
 
 
 @dataclass_json
 @dataclass
 class ServiceRequest:
-    """Response from service."""
+    """Request from service."""
 
     request_id: int
     message: str
+    town: Optional[str] = None
+    location: Optional[GeocodedAddress] = None
 
 
 @dataclass_json
@@ -36,7 +50,10 @@ class ServiceRequest:
 class ServiceResponseBody:
     address: str
     name: str
-    type: str
+    rubrics: List[str]
+    rating: float
+    text: str
+    geocode: Optional[GeocodedAddress] = None
 
 
 @dataclass_json
@@ -45,4 +62,4 @@ class ServiceResponse:
     """Response from service."""
 
     request_id: int
-    body: ServiceResponseBody
+    body: List[ServiceResponseBody]
--- nlp-service/main.py
+++ nlp-service/main.py
@@ -5,7 +5,7 @@ import os
 from common import (
     INPUT_QUEUE_NAME_ENVAR_KEY_NAME,
     RMQ_URL_ENVAR_KEY_NAME,
-    RMQ_INPUT_PORT_ENVAR_KEY_NAME,
+    RMQ_PORT_ENVAR_KEY_NAME,
     RMQ_PASSWORD_ENVAR_KEY_NAME,
     RMQ_USERNAME_ENVAR_KEY_NAME,
     OUTPUT_QUEUE_NAME_ENVAR_KEY_NAME,
@@ -15,14 +15,40 @@ from common import (
     QDRANT_COLLECTION_NAME_ENVAR_KEY_NAME,
     RESPONSE_ADDRESS_KEY,
     RESPONSE_TYPE_KEY,
-    RMQ_OUTPUT_PORT_ENVAR_KEY_NAME,
     ServiceResponse,
     ServiceResponseBody,
-    ServiceRequest,
+    ServiceRequest, GeocodedAddress, RESPONSE_NAME_KEY, RESPONSE_RATING_KEY, RESPONSE_TEXT_KEY,
 )
 from sentence_transformers import SentenceTransformer
-from typing import List
+from typing import List, Optional
 from qdrant_client import QdrantClient
+from qdrant_client.http.models import Filter, FieldCondition, MatchText
+
+import http.client
+from urllib.parse import quote
+
+
+def find_geocode_distance(goal: GeocodedAddress, given_coord: Optional[GeocodedAddress]):
+    """
+    Calculation is taken from
+    https://medium.com/analytics-vidhya/finding-nearest-pair-of-latitude-and-longitude-match-using-python-ce50d62af546
+    """
+    from math import radians, cos, sin, asin, sqrt
+    # Big value returned in case input coord is None.
+    BIG_GEOCODE_DISTANCE = 1000
+
+    if given_coord is None:
+        return BIG_GEOCODE_DISTANCE
+
+    earth_radius_km = 6371
+    goal_longitude, goal_latitude = map(radians, [goal.longitude, goal.latitude])
+    specific_longitude, specific_latitude = map(radians, [given_coord.longitude, given_coord.latitude])
+    dlon = specific_longitude - goal_longitude
+    dlat = specific_latitude - goal_latitude
+    a = sin(dlat / 2) ** 2 + cos(goal_latitude) * cos(specific_latitude) * sin(dlon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    km_distance = earth_radius_km * c
+    return km_distance
 
 
 class PythonService:
@@ -31,44 +57,40 @@ class PythonService:
         rmq_url = os.getenv(RMQ_URL_ENVAR_KEY_NAME)
         rmq_username = os.getenv(RMQ_USERNAME_ENVAR_KEY_NAME)
         rmq_password = os.getenv(RMQ_PASSWORD_ENVAR_KEY_NAME)
+        rmq_port = int(os.getenv(RMQ_PORT_ENVAR_KEY_NAME))
 
-        # Input queue configuration.
-        rmq_input_port = int(os.getenv(RMQ_INPUT_PORT_ENVAR_KEY_NAME))
-        input_connection = pika.BlockingConnection(
-            pika.ConnectionParameters(
-                host=rmq_url,
-                port=rmq_input_port,
-                credentials=pika.credentials.PlainCredentials(
-                    username=rmq_username, password=rmq_password
-                ),
-            )
-        )
-        rmq_input_channel = input_connection.channel()
-
-        input_queue_name = os.getenv(INPUT_QUEUE_NAME_ENVAR_KEY_NAME)
-        rmq_input_channel.queue_declare(queue=input_queue_name, durable=True)
-        rmq_input_channel.basic_qos(prefetch_count=1)
-        rmq_input_channel.basic_consume(
-            queue=input_queue_name, on_message_callback=self.handle_request_callback
-        )
-        self.rmq_input_channel = rmq_input_channel
-
-        # Output queue configuration.
-        rmq_output_port = int(os.getenv(RMQ_OUTPUT_PORT_ENVAR_KEY_NAME))
         output_connection = pika.BlockingConnection(
             pika.ConnectionParameters(
                 host=rmq_url,
-                port=rmq_output_port,
+                port=rmq_port,
                 credentials=pika.credentials.PlainCredentials(
                     username=rmq_username, password=rmq_password
                 ),
             )
         )
         rmq_output_channel = output_connection.channel()
-
         output_queue_name = os.getenv(OUTPUT_QUEUE_NAME_ENVAR_KEY_NAME)
         rmq_output_channel.queue_declare(queue=output_queue_name, durable=True)
         self.rmq_output_channel = rmq_output_channel
+
+        input_connection = pika.BlockingConnection(
+            pika.ConnectionParameters(
+                host=rmq_url,
+                port=rmq_port,
+                credentials=pika.credentials.PlainCredentials(
+                    username=rmq_username, password=rmq_password
+                ),
+            )
+        )
+        rmq_input_channel = input_connection.channel()
+        input_queue_name = os.getenv(INPUT_QUEUE_NAME_ENVAR_KEY_NAME)
+        rmq_input_channel.queue_declare(queue=input_queue_name, durable=True)
+        rmq_input_channel.basic_consume(
+            queue=input_queue_name,
+            on_message_callback=self.handle_request_callback,
+            auto_ack=True
+        )
+        self.rmq_input_channel = rmq_input_channel
 
         # ============ Quadrant configuration ==============
         self.transformer_model = SentenceTransformer(
@@ -77,7 +99,7 @@ class PythonService:
 
         qdrant_url = os.getenv(QDRANT_URL_ENVAR_KEY_NAME)
         qdrant_port = os.getenv(QDRANT_HTTP_PORT_ENVAR_KEY_NAME)
-        qdrant_gprc_port = os.getenv(QDRANT_GRPC_PORT_ENVAR_KEY_NAME)
+        qdrant_gprc_port = int(os.getenv(QDRANT_GRPC_PORT_ENVAR_KEY_NAME))
         qdrant_client = QdrantClient(
             host=qdrant_url,
             port=qdrant_port,
@@ -90,54 +112,86 @@ class PythonService:
         qdrant_collection_name = os.getenv(QDRANT_COLLECTION_NAME_ENVAR_KEY_NAME)
         self.qdrant_collection_name = qdrant_collection_name
 
-    def search_db(self, message) -> List[ServiceResponseBody]:
+        # ============ Geocoder configuration ==============
+        self.encoder_connection = http.client.HTTPSConnection('geocode.maps.co')
+
+    def transform_address_into_coordinates(self, address: str) -> Optional[GeocodedAddress]:
+        query_params = []
+        for address in address.split(", "):
+            # `quote` used to encode russian symbols.
+            query_params.extend([quote(single_param) for single_param in address.split(" ")])
+        address_as_param = "+".join(query_params)
+        self.encoder_connection.request("GET", f"/search?q={address_as_param}")
+
+        response = self.encoder_connection.getresponse()
+        if response.status == 429:
+            # We've exceeded requests per second limit. We may wait for some time and recursively call ourselves.
+            return None
+
+        data = response.read()
+        deserialized_data = json.loads(data)
+        if len(deserialized_data) >= 1:
+            nearest_res = deserialized_data[0]
+            return GeocodedAddress(float(nearest_res["lat"]), float(nearest_res["lon"]))
+        else:
+            # Server doesn't found anything.
+            return None
+
+    def search_db(self, message, town_filter_name=None) -> List[ServiceResponseBody]:
         embedding = self.transformer_model.encode([message])[0]
+
+        if town_filter_name is None:
+            query_filter = None
+        else:
+            query_filter = Filter(must=[FieldCondition(
+                key=RESPONSE_ADDRESS_KEY,
+                match=MatchText(text=town_filter_name),
+            )])
         search_result = self.qdrant_client.search(
             collection_name=self.qdrant_collection_name,
             query_vector=embedding,
-            query_filter=None,
+            query_filter=query_filter,
         )
 
         response = []
         for res in search_result:
             res_payload = res.payload
+
+            res_payload_address = res_payload[RESPONSE_ADDRESS_KEY]
+            # address_geocode = self.transform_address_into_coordinates(res_payload_address)
             response.append(
                 ServiceResponseBody(
-                    res_payload[RESPONSE_ADDRESS_KEY],
-                    "Some name",
+                    res_payload_address,
+                    res_payload[RESPONSE_NAME_KEY],
                     res_payload[RESPONSE_TYPE_KEY],
+                    res_payload[RESPONSE_RATING_KEY],
+                    res_payload[RESPONSE_TEXT_KEY],
+                    None
                 )
             )
         return response
 
     def handle_request_callback(self, ch, method, properties, body):
         request = ServiceRequest(**json.loads(body))
-
         print(f"Successfully get message: [{request}]")
 
         quadrant_response = self.search_db(request.message)
-        response_dataclasses = [
-            ServiceResponse(request.request_id, res_body)
-            for res_body in quadrant_response
-        ]
+        # if request.location is not None:
+        #     response_dataclasses.sort(key=lambda res: find_geocode_distance(request.location, res.))
+        response = ServiceResponse(request.request_id, quadrant_response)
         serialized_response = ServiceResponse.schema().dumps(
-            response_dataclasses, many=True
+            response, many=False
         )
 
+        output_queue_name = os.getenv(OUTPUT_QUEUE_NAME_ENVAR_KEY_NAME)
         self.rmq_output_channel.basic_publish(
-            exchange="",
-            routing_key=OUTPUT_QUEUE_NAME_ENVAR_KEY_NAME,
-            body=serialized_response,
-            properties=pika.BasicProperties(
-                delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
-            ),
+            exchange='',
+            routing_key=output_queue_name,
+            body=serialized_response
         )
-        ch.basic_ack(delivery_tag=method.delivery_tag)
-        print(f"Successfully sent response: [{serialized_response}]")
 
     def start_listening_input_query(self):
         self.rmq_input_channel.start_consuming()
-
 
 if __name__ == "__main__":
     service = PythonService()
