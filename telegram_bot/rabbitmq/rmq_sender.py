2f3bfd3165adb1f483d9fc78ebcd037becf47ff7
makar-pelogeiko <m.pelogeiko@mail.ru>
2023-11-16 17:42:05 +0300

    Feature(RabbitMQ tgbot) - connection retry added

--- telegram_bot/rabbitmq/rmq_sender.py
+++ telegram_bot/rabbitmq/rmq_sender.py
@@ -14,11 +14,13 @@ class RMQSender:
         user: str,
         password: str,
     ) -> None:
+        self.retries = 0
+        self.max_retries = 5
         self.queue = queue
         self.connection = pika.BlockingConnection(
             pika.ConnectionParameters(
                 host=host,
-                port=port,
+                port=int(port),
                 credentials=pika.PlainCredentials(username=user,
                                                   password=password),
             ))
@@ -27,15 +29,31 @@ class RMQSender:
 
     def send_message(self, request_id: int, msg: str, town: Optional[str]) -> None:
         message = {'request_id': request_id, 'message': msg, "town": town}
-        self.channel.basic_publish(exchange='',
-                                   routing_key=self.queue,
-                                   body=json.dumps(message),
-                                   properties=pika.BasicProperties(
-                                       content_encoding='utf-8',
-                                       delivery_mode=1,
-                                   ))
+        if self.retries > self.max_retries:
+            print(f'too much fails')
+            self.channel.basic_publish(exchange='',
+                                       routing_key=self.queue,
+                                       body=json.dumps(message),
+                                       properties=pika.BasicProperties(
+                                           content_encoding='utf-8',
+                                           delivery_mode=1,
+                                       ))
+
+        try:
+            self.channel.basic_publish(exchange='',
+                                       routing_key=self.queue,
+                                       body=json.dumps(message),
+                                       properties=pika.BasicProperties(
+                                           content_encoding='utf-8',
+                                           delivery_mode=1,
+                                       ))
+        except:
+            print(f"exception while message publishing")
+            self.retries += 1
+            self.send_message(request_id, msg, town)
         print(f"Sent message to the input queue: [{msg}]")
         print(f"Input queue name: [{self.queue}]")
+        self.retries = 0
 
     def close_connection(self) -> None:
         self.connection.close()
